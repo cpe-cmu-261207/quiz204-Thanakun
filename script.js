ae5743943720b383b3f98a28bc5d58d37861f1dd
Thanakun <Thanakun_Ch@cmu.ac.th>
2021-07-29 14:19:41 +0700

    Saddd

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const btn = document.querySelector("button")
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
   const ans = 630610733 - length.value;
-  author.innerHTML = ans;
-
+  author.style.display = "none";
+  calculation.innerHTML = ans;
+  btn_toggle.innerHTML = "Display author"
+  // if(btn_toggle.innerHTML === "Display author"){
+    
+  // }
 }
 
+
 // more codes for Search and Reset buttons here
+
